6c0ff1681d5bbd378721958fdca6030b7eda2936
bezbowie <bezbowie@gmail.com>
2019-05-19 22:05:37 +0800

    Revert "MAP WILL GET IT'S LOCATION TO THE LOCATION OF THE USER AND MAKING IT WORLD WIDE USABLE"
    This reverts commit ccca1e51ab71a8d127f45cc177850bf99e48eccc.

--- app/src/main/java/com/gigfindr/admin/app/MapActivity.java
+++ app/src/main/java/com/gigfindr/admin/app/MapActivity.java
@@ -174,8 +174,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         mMap = googleMap;
         //zoom camera to singapore
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(1.3521, 103.8198)));
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(1.3521, 103.8198)));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(11.0f));
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
 
@@ -420,7 +419,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
                 currentLat = String.valueOf(location.getLatitude());
                 currentLong = String.valueOf(location.getLongitude());
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(Double.parseDouble(currentLat), Double.parseDouble(currentLong))));
                 currentLocation.setLatitude(Double.parseDouble(currentLat));
                 currentLocation.setLongitude(Double.parseDouble(currentLong));
 
